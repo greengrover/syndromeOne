31ef79faac11d01aa17f85a6ba9c6a1b924ced90
greengrover <nikonovbox@gmail.com>
2021-09-05 12:25:53 +0300

    add spotbugs

--- src/main/java/ru/netology/syndrome/SyndromeService.java
+++ src/main/java/ru/netology/syndrome/SyndromeService.java
@@ -2,6 +2,7 @@ package ru.netology.syndrome;
 
 public class SyndromeService {
     public long findMax(long[] incomesInBillions) {
+//        long sumIncomes = sum(incomesInBillions); ADD CODE FOR CHECK SPOTBUGS PLUG_IN
         long current_max_index = 0;
         long current_max = incomesInBillions[0];
         for (long incomesInBillion : incomesInBillions) {
@@ -11,4 +12,13 @@ public class SyndromeService {
         }
         return current_max;
     }
+
+// ADD CODE FOR CHECK SPOTBUGS PLUG_IN
+//    private long sum(long[] incomesInBillions) {
+//        long sumIncomes = 0;
+//        for (long incomesInBillion : incomesInBillions) {
+//            sumIncomes = sumIncomes + incomesInBillion;
+//        }
+//        return sumIncomes;
+//    }
 }
