6afca33057744f6859037a1d0e78c36c2bd8cb7f
greengrover <nikonovbox@gmail.com>
2021-09-05 11:27:16 +0300

    change tests

--- src/test/java/ru/netology/syndrome/SyndromeServiceTest.java
+++ src/test/java/ru/netology/syndrome/SyndromeServiceTest.java
@@ -9,8 +9,8 @@ class SyndromeServiceTest {
     @Test
     void findMax() {
         SyndromeService service = new SyndromeService();
-        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
-        long expected = 12;
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 13};
+        long expected = 13;
         long actual = service.findMax(incomesInBillions);
         assertEquals(expected, actual);
 
